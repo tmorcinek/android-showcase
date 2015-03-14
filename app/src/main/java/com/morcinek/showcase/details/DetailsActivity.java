f16aa2a403a33c1bbc8cf0e746798de36815e70b
Tomasz Morcinek <tomasz@morcinek.co.uk>
2015-03-14 20:36:02 +0000

    Refactoring of DetailsActivity.

--- app/src/main/java/com/morcinek/showcase/details/DetailsActivity.java
+++ app/src/main/java/com/morcinek/showcase/details/DetailsActivity.java
@@ -1,8 +1,6 @@
 package com.morcinek.showcase.details;
 
 import android.os.Bundle;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
@@ -27,24 +25,25 @@ public class DetailsActivity extends ShowcaseActivity implements View.OnClickLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         overridePendingTransition(R.anim.slide_in_top, android.R.anim.fade_out);
-        setContentView(R.layout.home);
-
-        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        setContentView(R.layout.details);
 
         setupToolbar();
         String className = getIntent().getStringExtra(Class.class.getName());
         if (className.equals(EducationDetailsFragment.class.getName())) {
-            Education education = getIntent().getParcelableExtra(Object.class.getName());
-            homeContentController.addFragment(EducationDetailsFragment.newInstance(education));
+            setupEducationDetailsFragment();
         }
+    }
+
+    private void setupEducationDetailsFragment() {
+        Education education = getIntent().getParcelableExtra(Object.class.getName());
+        getSupportActionBar().setTitle(education.getUniversity());
+        homeContentController.addFragment(EducationDetailsFragment.newInstance(education));
     }
 
     private void setupToolbar() {
         final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setTitle("");
         toolbar.setBackgroundColor(getResources().getColor(android.R.color.transparent));
         toolbar.setNavigationOnClickListener(this);
     }
