cf466145db163b7ca6e9ea83414e1bcfa91b45e2
Tomasz Morcinek <tomasz@morcinek.co.uk>
2015-03-26 19:42:26 +0000

    Fixing drawer item layout.

--- app/src/main/java/com/morcinek/showcase/home/navigation/drawer/DrawerListAdapter.java
+++ app/src/main/java/com/morcinek/showcase/home/navigation/drawer/DrawerListAdapter.java
@@ -3,6 +3,7 @@ package com.morcinek.showcase.home.navigation.drawer;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.morcinek.showcase.R;
@@ -22,22 +23,29 @@ public class DrawerListAdapter extends AbstractListAdapter<DrawerItem> {
         View view = convertView;
         if (view == null) {
             view = getLayoutInflater().inflate(R.layout.drawer_item, parent, false);
-            ViewHolder viewHolder = new ViewHolder();
-            viewHolder.textView = (TextView) view;
+            ViewHolder viewHolder = createViewHolder(view);
             view.setTag(viewHolder);
         }
         initializeViews(getItem(position), (ViewHolder) view.getTag());
         return view;
     }
 
+    private ViewHolder createViewHolder(View view) {
+        ViewHolder viewHolder = new ViewHolder();
+        viewHolder.title = (TextView) view.findViewById(R.id.title);
+        viewHolder.icon = (ImageView) view.findViewById(R.id.icon);
+        return viewHolder;
+    }
+
     private void initializeViews(DrawerItem drawerItem, ViewHolder holder) {
-        holder.textView.setText(drawerItem.getTitle());
+        holder.title.setText(drawerItem.getTitle());
         if (drawerItem.getIcon() != null) {
-            holder.textView.setCompoundDrawablesWithIntrinsicBounds(drawerItem.getIcon(), 0, 0, 0);
+            holder.icon.setImageResource(drawerItem.getIcon());
         }
     }
 
     protected class ViewHolder {
-        public TextView textView;
+        private TextView title;
+        private ImageView icon;
     }
 }
